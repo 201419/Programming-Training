385a9d8dcba62fef43defd0eadbe840f735c5281
201419 <1275460343@qq.com>
2018-02-13 20:46:12 +0800

    2018/02/13 the 2-th commit

---
+++ Leetcode/0003LongestSubstringWithoutRepeatingCharacters.cpp
@@ -0,0 +1,16 @@
+// 区区9行就实现了要求，而且时间复杂度为o(n)，简直是神来之笔
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> dict(256, -1);
+        int maxLen = 0, start = -1;
+        for (int i = 0; i != s.length(); i++) {
+            if (dict[s[i]] > start)
+                start = dict[s[i]];
+            dict[s[i]] = i;
+            maxLen = max(maxLen, i - start);
+        }
+        return maxLen;
+    }
+};
