d7566229d9d35bbf35a844d93ce7618c102a4695
201419 <yangshu1109@gmail.com>
2019-06-19 13:47:10 +0800

    update program-training 20190619 -03

--- Leetcode/0011-0020.cpp
+++ Leetcode/0011-0020.cpp
@@ -160,6 +160,42 @@ public:
 // https://leetcode-cn.com/problems/4sum/
 // 18. 四数之和
 
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size();
+		if (n < 4) return {};
+		sort(nums.begin(), nums.end());
+        
+		vector<vector<int>> res;
+		for (int i = 0; i < n - 3; i++) {
+			if (i > 0 && nums[i] == nums[i - 1]) continue;
+			if (nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) break;
+			if (nums[i] + nums[n - 3] + nums[n - 2] + nums[n - 1] < target) continue;
+ 
+			for (int j = i + 1; j < n - 2; j++) {
+				if (j > i + 1 && nums[j] == nums[j - 1])continue;
+				if (nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) break;
+				if (nums[i] + nums[j] + nums[n - 2] + nums[n - 1] < target) continue;
+				
+				int l = j + 1, r = n - 1;
+				while (l < r) {
+					int sum = nums[i] + nums[j] + nums[l] + nums[r];
+                    
+                    if (sum < target) l++;
+                    else if (sum > target) r--;
+                    else {
+                        res.push_back({nums[i], nums[j], nums[l++], nums[r--]});
+						while (l < r && nums[l] == nums[l - 1]) l++;
+						while (l < r && nums[r] == nums[r + 1]) r--;
+                    }
+				}
+			}
+		}
+		return res;
+    }
+};
+
 // ---------------------------------------------------------------------------
 
 // https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/
