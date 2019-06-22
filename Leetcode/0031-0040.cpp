fc0c6c539da767b5e1f43cbe56d4d1492ae363b6
201419 <yangshu1109@gmail.com>
2019-06-22 23:18:12 +0800

    update program-training 20190622 -02

--- Leetcode/0031-0040.cpp
+++ Leetcode/0031-0040.cpp
@@ -8,12 +8,65 @@
 // https://leetcode-cn.com/problems/search-in-rotated-sorted-array/
 // 33. 搜索旋转排序数组
 
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        return search(nums, 0, nums.size()-1, target);
+    }
+private:
+    int search(vector<int>& nums, int low, int high, int target) {
+        if (low > high) return -1;
+        int mid = (low + high) / 2;
+        if (nums[mid] == target) return mid;   
+        if (nums[mid] < nums[high]) {
+            if (nums[mid] < target && target <= nums[high]) return search(nums, mid + 1, high, target);
+            else return search(nums, low, mid - 1, target);
+        } else {
+            if (nums[low] <= target && target < nums[mid]) return search(nums, low, mid - 1, target);
+            else return search(nums, mid + 1, high, target);
+        }
+    }
+};
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1, mid = 0;
+        while (low <= high) {
+            mid = (low + high) / 2;
+            if (nums[mid] == target) return mid;       
+            if (nums[mid] < nums[high]) {
+                if (nums[mid] < target && target <= nums[high]) low = mid + 1;
+                else high = mid - 1;
+            } else {
+                if (nums[low] <= target && target < nums[mid]) high = mid - 1;
+                else low = mid + 1;
+            }
+        }
+        return -1;
+    }
+};
+
 // ---------------------------------------------------------------------------
 
 // ---------------------------------------------------------------------------
 
 // https://leetcode-cn.com/problems/search-insert-position/
 // 35. 搜索插入位置
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] < target) { low = mid + 1; }
+            else if (nums[mid] > target) { high = mid - 1; }
+            else { return mid; }
+        }
+        return low;
+    }
+};
 
 // ---------------------------------------------------------------------------
 
