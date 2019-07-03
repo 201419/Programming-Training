0d68dbfad0c9287e8649db9b1b9a6ed4b44a337b
201419 <yangshu1109@gmail.com>
2019-07-03 09:29:24 +0800

    update program-training 20190703 -03

--- Leetcode/0341-0350.cpp
+++ Leetcode/0341-0350.cpp
@@ -55,24 +55,80 @@ public:
 
 // ---------------------------------------------------------------------------
 
+// https://leetcode-cn.com/problems/reverse-vowels-of-a-string/
+// 345. 反转字符串中的元音字母
 
+class Solution {
+public:
+    string reverseVowels(string s) {
+        int left = 0, right = s.size() - 1;
+        while (left < right) {
+            if (!isVowel(s[left])) { left ++; }
+            else if (!isVowel(s[right])) { right --; }
+            else {
+                swap(s[left], s[right]);
+                left ++;
+                right --;
+            }
+        }
+        return s;
+    }
+    
+    bool isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' 
+            || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+    }
+};
 
 // ---------------------------------------------------------------------------
 
-
-
-// ---------------------------------------------------------------------------
-
-
+// https://leetcode-cn.com/problems/moving-average-from-data-stream/
+// 346. 数据流中的移动平均值
 
 // ---------------------------------------------------------------------------
 
-
-
-// ---------------------------------------------------------------------------
-
-
+// https://leetcode-cn.com/problems/top-k-frequent-elements/
+// 347. 前K个高频元素
 
 // ---------------------------------------------------------------------------
 
+// https://leetcode-cn.com/problems/design-tic-tac-toe/
+// 348. 判定井字棋胜负
 
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/intersection-of-two-arrays/
+// 349. 两个数组的交集
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> hashSet(nums1.begin(), nums1.end());
+        unordered_set<int> hashSet2;
+        for(const auto &num : nums2) {
+            if(hashSet.count(num) > 0) hashSet2.insert(num);
+        }
+        return vector<int>(hashSet2.begin(), hashSet2.end());
+    }
+};
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/intersection-of-two-arrays-ii/
+// 350. 两个数组的交集 II
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        map<int,int> record;
+        vector<int> res;
+        for (int i = 0; i < nums1.size(); i++) record[nums1[i]]++;
+        for (int i = 0; i < nums2.size(); i++) {
+            if (record[nums2[i]] > 0) {
+                res.push_back(nums2[i]);
+                record[nums2[i]]--;
+            }
+        }
+        return res;
+    }
+};
