2984eb64f2795e12dd70d799602d38bd8eb46786
201419 <yangshu1109@gmail.com>
2019-07-08 15:29:39 +0800

    update program-training 20190708 -02

--- Leetcode/0121-0130.cpp
+++ Leetcode/0121-0130.cpp
@@ -19,11 +19,24 @@ public:
 
 // ---------------------------------------------------------------------------
 
+// https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/
+// 122. 买卖股票的最佳时机 II
 
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit = 0;
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i-1] < prices[i])  maxProfit += prices[i] - prices[i-1];
+        }
+        return maxProfit;
+    }
+};
 
 // ---------------------------------------------------------------------------
 
-
+// https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-iii/
+// 123. 买卖股票的最佳时机 III
 
 // ---------------------------------------------------------------------------
 
