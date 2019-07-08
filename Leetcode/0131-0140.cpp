9ec36eb88588a2a730e9304801af2a4cdfef30e6
201419 <yangshu1109@gmail.com>
2019-07-08 15:42:13 +0800

    update program-training 20190708 -05

--- Leetcode/0131-0140.cpp
+++ Leetcode/0131-0140.cpp
@@ -23,16 +23,36 @@
 
 // ---------------------------------------------------------------------------
 
+// https://leetcode-cn.com/problems/single-number/
+// 136. 只出现一次的数字
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int result = 0;
+        for(int i = 0; i < n; i++){
+            result ^= nums[i];
+        }
+        return result;
+    }
+};
 
 // ---------------------------------------------------------------------------
 
+// https://leetcode-cn.com/problems/single-number-ii/
+// 137. 只出现一次的数字 II
 
 // ---------------------------------------------------------------------------
 
+// https://leetcode-cn.com/problems/copy-list-with-random-pointer/
+// 138. 复制带随机指针的链表
 
 // ---------------------------------------------------------------------------
 
+// https://leetcode-cn.com/problems/word-break/
+// 139. 单词拆分
 
 // ---------------------------------------------------------------------------
 
-
+// https://leetcode-cn.com/problems/word-break-ii/
+// 140. 单词拆分 II
