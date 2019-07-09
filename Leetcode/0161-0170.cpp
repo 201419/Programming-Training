a4d5a7358c841c4f3ce7b826bd574a0129276b83
201419 <yangshu1109@gmail.com>
2019-07-09 11:25:40 +0800

    update program-training 20190709 -05

--- Leetcode/0161-0170.cpp
+++ Leetcode/0161-0170.cpp
@@ -28,11 +28,74 @@
 
 // ---------------------------------------------------------------------------
 
+// https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted/
+// 167. 两数之和 II - 输入有序数组
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> result(2);
+        int low = 0, high = numbers.size() - 1;
+        while (low < high) {
+            if (numbers[low] + numbers[high] < target) { low++; }
+            else if (numbers[low] + numbers[high] > target) { high--; }
+            else {
+                result[0] = low + 1;
+                result[1] = high + 1;
+                return result;
+            }
+        }
+        return result;
+    }
+};
 
 // ---------------------------------------------------------------------------
 
+// https://leetcode-cn.com/problems/excel-sheet-column-title/
+// 168. Excel表列名称
 
 // ---------------------------------------------------------------------------
 
+// https://leetcode-cn.com/problems/majority-element/
+// 169. 求众数
+
+// 40 ms
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count = 1;
+		int majority = nums[0];
+		for (int i = 1; i < nums.size(); i++) {
+			if (majority == nums[i]) count++;
+			else {
+				count--;
+				if (count == 0) majority = nums[i + 1];
+			}
+		}
+		return majority;
+    }
+};
+
+// 24 ms
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int result = nums[0], cnt = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == result) { cnt++; }
+            else if (cnt == 0) {
+                result = nums[i];
+                cnt++;
+            }
+            else { cnt--; }
+        }
+        return result;
+    }
+};
 
 // ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/two-sum-iii-data-structure-design/
+// 170. 两数之和 III - 数据结构设计
