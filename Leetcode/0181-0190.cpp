9e7455dddfa7b086fb15f09c122e60a2af2f0971
201419 <yangshu1109@gmail.com>
2019-07-10 10:51:47 +0800

    update program-training 20190710 -07

--- Leetcode/0181-0190.cpp
+++ Leetcode/0181-0190.cpp
@@ -41,6 +41,71 @@
 // https://leetcode-cn.com/problems/rotate-array/
 // 189. 旋转数组
 
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k = k % nums.size();
+        if (k != 0) {
+            reverse(nums, 0, nums.size() - 1);
+            reverse(nums, 0, k - 1);
+            reverse(nums, k, nums.size() - 1);
+        }
+    }
+    
+    void reverse(vector<int>& nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start++] = nums[end];
+            nums[end--] = temp;
+        }
+    }
+};
+
+// other method
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k = k % nums.size();
+        if (k <= 0) return;
+
+        int target = 1;
+        if (nums.size() % k == 0) {
+            target = k;
+        } else {
+            for (int i = 2; i <= k / 2; i++) {
+                if ((nums.size() % i == 0) && (k %  i) == 0) {
+                    target = i;
+                }
+            }
+        }
+
+        for (int i = 0; i < target; i++) {
+            int curr = (i + k) % nums.size();
+            int pre = nums[i];
+            while (curr != i) {
+                int temp = nums[curr];
+                nums[curr] = pre;
+                pre = temp;
+                curr = (curr + k) % nums.size();
+            }
+            nums[i] = pre;
+        }
+    }
+};
+
+// 空间换时间
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        vector<int> tmp=nums;
+        for(int i=0;i<nums.size();++i) {
+            nums[(i+k)%nums.size()]=tmp[i];
+        }
+    }
+};
+
 // ---------------------------------------------------------------------------
 
 // https://leetcode-cn.com/problems/reverse-bits/
