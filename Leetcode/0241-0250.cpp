72bbb88a76d7b32dd425c95b21b2c0963aebfce1
201419 <yangshu1109@gmail.com>
2019-07-12 09:06:28 +0800

    update program-training 20190712 -02

--- Leetcode/0241-0250.cpp
+++ Leetcode/0241-0250.cpp
@@ -31,6 +31,26 @@ public:
 
 // ---------------------------------------------------------------------------
 
+// https://leetcode-cn.com/problems/valid-anagram/
+// 242. 有效的字母异位词
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        
+        vector<int> num(26, 0);
+        for (int i = 0; i < s.length(); i++) {
+            num[s[i]-'a']++;
+            num[t[i]-'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (num[i] != 0) return false;
+        }
+            
+        return true;
+    }
+};
 
 // ---------------------------------------------------------------------------
 
