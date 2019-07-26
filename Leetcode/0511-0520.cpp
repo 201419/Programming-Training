05d6b59b4a1c90473e0624779f6ab327aee1823a
201419 <yangshu1109@gmail.com>
2019-07-26 09:46:50 +0800

    update coding 20190726 -05

--- Leetcode/0511-0520.cpp
+++ Leetcode/0511-0520.cpp
@@ -1,3 +1,59 @@
 
+// https://leetcode-cn.com/problems/game-play-analysis-i/
+// 511. Game Play Analysis I
+
+
 // ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/game-play-analysis-ii/
+// 512. Game Play Analysis II
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/find-bottom-left-tree-value/
+// 513. 找树左下角的值
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/freedom-trail/
+// 514. 自由之路
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/find-largest-value-in-each-tree-row/
+// 515. 在每个树行中找最大值
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/longest-palindromic-subsequence/
+// 516. 最长回文子序列
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/super-washing-machines/
+// 517. 超级洗衣机
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/coin-change-2/
+// 518. 零钱兑换 II
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/random-flip-matrix/
+// 519. 随机翻转矩阵
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/detect-capital/
+// 520. 检测大写字母
+
 
