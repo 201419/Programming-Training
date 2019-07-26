c2cf56d9a4acf43bad107b9dc89e4852aba7d079
201419 <yangshu1109@gmail.com>
2019-07-26 09:47:43 +0800

    update coding 20190726 -06

--- Leetcode/0521-0530.cpp
+++ Leetcode/0521-0530.cpp
@@ -1,3 +1,60 @@
 
+// https://leetcode-cn.com/problems/longest-uncommon-subsequence-i/
+// 521. 最长特殊序列 Ⅰ
+
+
 // ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/longest-uncommon-subsequence-ii/
+// 522. 最长特殊序列 II
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/continuous-subarray-sum/
+// 523. 连续的子数组和
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/longest-word-in-dictionary-through-deleting/
+// 524. 通过删除字母匹配到字典里最长单词
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/contiguous-array/
+// 525. 连续数组
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/beautiful-arrangement/
+// 526. 优美的排列
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/word-abbreviation/
+// 527. 单词缩写
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/random-pick-with-weight/
+// 528. 按权重随机选择
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/minesweeper/
+// 529. 扫雷游戏
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/minimum-absolute-difference-in-bst/
+// 530. 二叉搜索树的最小绝对差
+
+
 
--- Leetcode/0531-0540.cpp
+++ Leetcode/0531-0540.cpp
@@ -1,3 +1,60 @@
 
+// https://leetcode-cn.com/problems/lonely-pixel-i/
+// 531. 孤独像素 I
+
+
 // ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/k-diff-pairs-in-an-array/
+// 532. 数组中的K-diff数对
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/lonely-pixel-ii/
+// 533. 孤独像素 II
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/game-play-analysis-iii/
+// 534. Game Play Analysis III
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/encode-and-decode-tinyurl/
+// 535. TinyURL 的加密与解密
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/construct-binary-tree-from-string/
+// 536. 从字符串生成二叉树
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/complex-number-multiplication/
+// 537. 复数乘法
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/convert-bst-to-greater-tree/
+// 538. 把二叉搜索树转换为累加树
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/minimum-time-difference/
+// 539. 最小时间差
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/single-element-in-a-sorted-array/
+// 540. 有序数组中的单一元素
+
+
 
