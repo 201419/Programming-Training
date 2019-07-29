3282718c612fdb8bda957f11f918868b6ab8d2ff
201419 <yangshu1109@gmail.com>
2019-07-29 10:32:18 +0800

    update coding 20190729 -03

--- Leetcode/0561-0570.cpp
+++ Leetcode/0561-0570.cpp
@@ -1,3 +1,62 @@
 
+// https://leetcode-cn.com/problems/array-partition-i/
+// 561. 数组拆分 I
+
+
 // ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/longest-line-of-consecutive-one-in-matrix/
+// 562. 矩阵中最长的连续1线段
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/binary-tree-tilt/
+// 563. 二叉树的坡度
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/find-the-closest-palindrome/
+// 564. 寻找最近的回文数
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/array-nesting/
+// 565. 数组嵌套
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/reshape-the-matrix/
+// 566. 重塑矩阵
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/permutation-in-string/
+// 567. 字符串的排列
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/maximum-vacation-days/
+// 568. 最大休假天数
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/median-employee-salary/
+// 569. 员工薪水中位数
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/managers-with-at-least-5-direct-reports/
+// 570. 至少有5名直接下属的经理
+
+
+
+
 
