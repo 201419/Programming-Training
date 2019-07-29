e4a3adccf95b104e7acab8fc2c6decb4c4542ec5
201419 <yangshu1109@gmail.com>
2019-07-29 10:33:08 +0800

    update coding 20190729 -04

--- Leetcode/0571-0580.cpp
+++ Leetcode/0571-0580.cpp
@@ -1,3 +1,60 @@
 
+// https://leetcode-cn.com/problems/find-median-given-frequency-of-numbers/
+// 571. 给定数字的频率查询中位数
+
+
 // ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/subtree-of-another-tree/
+// 572. 另一个树的子树
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/squirrel-simulation/
+// 573. 松鼠模拟
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/winning-candidate/
+// 574. 当选者
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/distribute-candies/
+// 575. 分糖果
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/out-of-boundary-paths/
+// 576. 出界的路径数
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/employee-bonus/
+// 577. 员工奖金
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/get-highest-answer-rate-question/
+// 578. 查询回答率最高的问题
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/find-cumulative-salary-of-an-employee/
+// 579. 查询员工的累计薪水
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/count-student-number-in-departments/
+// 580. 统计各专业学生人数
+
+
 
