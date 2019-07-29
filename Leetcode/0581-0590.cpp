be47493c5539b6a5e87a832001bd1f5d9a3a5241
201419 <yangshu1109@gmail.com>
2019-07-29 10:33:44 +0800

    update coding 20190729 -05

--- Leetcode/0581-0590.cpp
+++ Leetcode/0581-0590.cpp
@@ -1,3 +1,62 @@
 
+// https://leetcode-cn.com/problems/shortest-unsorted-continuous-subarray/
+// 581. 最短无序连续子数组
+
+
 // ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/kill-process/
+// 582. 杀死进程
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/delete-operation-for-two-strings/
+// 583. 两个字符串的删除操作
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/find-customer-referee/
+// 584. 寻找用户推荐人
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/investments-in-2016/
+// 585. 2016年的投资
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/customer-placing-the-largest-number-of-orders/
+// 586. 订单最多的客户
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/erect-the-fence/
+// 587. 安装栅栏
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/design-in-memory-file-system/
+// 588. 设计内存文件系统
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/n-ary-tree-preorder-traversal/
+// 589. N叉树的前序遍历
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/n-ary-tree-postorder-traversal/
+// 590. N叉树的后序遍历
+
+
+
+
 
