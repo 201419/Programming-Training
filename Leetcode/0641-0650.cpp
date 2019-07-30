f4abf3ce154c72bc5b082ac10c1982959078fc5f
201419 <yangshu1109@gmail.com>
2019-07-30 19:52:22 +0800

    update coding 20190730 -05

--- Leetcode/0641-0650.cpp
+++ Leetcode/0641-0650.cpp
@@ -1,3 +1,61 @@
 
+// https://leetcode-cn.com/problems/design-circular-deque/
+// 641. 设计循环双端队列
+
+
 // ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/design-search-autocomplete-system/
+// 642. 设计搜索自动补全系统
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/maximum-average-subarray-i/
+// 643. 子数组最大平均数 I
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/maximum-average-subarray-ii/
+// 644. 最大平均子段和 II
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/set-mismatch/
+// 645. 错误的集合
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/maximum-length-of-pair-chain/
+// 646. 最长数对链
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/palindromic-substrings/
+// 647. 回文子串
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/replace-words/
+// 648. 单词替换
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/dota2-senate/
+// 649. Dota2 参议院
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/2-keys-keyboard/
+// 650. 只有两个键的键盘
+
+
+
 
