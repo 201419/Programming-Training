912e4f3f85ca591027be82160316834fbc2ee886
201419 <yangshu1109@gmail.com>
2019-07-30 19:51:43 +0800

    update coding 20190730 -04

--- Leetcode/0631-0640.cpp
+++ Leetcode/0631-0640.cpp
@@ -1,3 +1,60 @@
 
+// https://leetcode-cn.com/problems/design-excel-sum-formula/
+// 631. 设计 Excel 求和公式
+
+
 // ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/smallest-range/
+// 632. 最小区间
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/sum-of-square-numbers/
+// 633. 平方数之和
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/find-the-derangement-of-an-array/
+// 634. 寻找数组的错位排列
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/design-log-storage-system/
+// 635. 设计日志存储系统
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/exclusive-time-of-functions/
+// 636. 函数的独占时间
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/average-of-levels-in-binary-tree/
+// 637. 二叉树的层平均值
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/shopping-offers/
+// 638. 大礼包
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/decode-ways-ii/
+// 639. 解码方法 2
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/solve-the-equation/
+// 640. 求解方程
+
+
 
