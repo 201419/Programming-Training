b1500abe769587890e86b06bb7bd022cbddd9dcd
201419 <yangshu1109@gmail.com>
2019-07-30 19:50:37 +0800

    update coding 20190730 -02

--- Leetcode/0611-0620.cpp
+++ Leetcode/0611-0620.cpp
@@ -1,3 +1,60 @@
 
+// https://leetcode-cn.com/problems/valid-triangle-number/
+// 611. 有效三角形的个数
+
+
 // ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/shortest-distance-in-a-plane/
+// 612. 平面上的最近距离
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/shortest-distance-in-a-line/
+// 613. 直线上的最近距离
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/second-degree-follower/
+// 614. 二级关注者
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/average-salary-departments-vs-company/
+// 615. 平均工资：部门与公司比较
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/add-bold-tag-in-string/
+// 616. 给字符串添加加粗标签
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/merge-two-binary-trees/
+// 617. 合并二叉树
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/students-report-by-geography/
+// 618. 学生地理信息报告
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/biggest-single-number/
+// 619. 只出现一次的最大数字
+
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/not-boring-movies/
+// 620. 有趣的电影
+
+
 
