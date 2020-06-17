0bd82b7b71e7e0445fe5e7ec54cf2ef15c595dd4
Shu Yang <yangshu1109@foxmail.com>
2020-06-17 10:21:54 +0800

    Update 013_调整数组顺序使奇数位于偶数前面.cpp

--- 剑指Offer编程66题/013_调整数组顺序使奇数位于偶数前面.cpp
+++ 剑指Offer编程66题/013_调整数组顺序使奇数位于偶数前面.cpp
@@ -4,7 +4,7 @@ public:
         vector<int>::iterator it = array.begin();
         int size = array.size();
         while(size--) {
-            if(*it%2==0) {
+            if(*it%2==0) {  // !(*it&1)
                 int tmp=*it;
                 it=array.erase(it);
                 array.push_back(tmp);
