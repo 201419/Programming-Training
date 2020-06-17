22aa47e2f749eec706af317f492e343e52a42512
Shu Yang <yangshu1109@foxmail.com>
2020-06-17 09:24:58 +0800

    Update 005_用两个栈实现队列.cpp

--- 剑指Offer编程66题/005_用两个栈实现队列.cpp
+++ 剑指Offer编程66题/005_用两个栈实现队列.cpp
@@ -23,3 +23,30 @@ private:
     stack<int> stack1;
     stack<int> stack2;
 };
+
+/* ============================================== */
+
+class Solution
+{
+public:
+    void push(int node) {
+        stack1.push(node);
+    }
+
+    int pop() {
+        if (stack2.empty()) {
+            while(!stack1.empty()) {
+                stack2.push(stack1.top());
+                stack1.pop();
+            }
+        }
+        
+        int ret=stack2.top();
+        stack2.pop();
+        return ret;
+    }
+
+private:
+    stack<int> stack1;
+    stack<int> stack2;
+};
