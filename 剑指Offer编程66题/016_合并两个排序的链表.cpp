a772d6f565be8422d7ad857db0a20bbf59cf333a
Shu Yang <yangshu1109@foxmail.com>
2020-06-17 10:51:31 +0800

    Update 016_合并两个排序的链表.cpp

--- 剑指Offer编程66题/016_合并两个排序的链表.cpp
+++ 剑指Offer编程66题/016_合并两个排序的链表.cpp
@@ -38,3 +38,35 @@ public:
         return mergeList;
     }
 };
+
+/* =========================================================== */
+
+/*
+struct ListNode {
+	int val;
+	struct ListNode *next;
+	ListNode(int x) :
+			val(x), next(NULL) {
+	}
+};*/
+class Solution {
+public:
+    ListNode* Merge(ListNode* pHead1, ListNode* pHead2)
+    {
+        if (!pHead1)  return pHead2;
+        else if (!pHead2)  return pHead1;
+        
+        ListNode* pMergeHead = NULL;
+        
+        if (pHead1->val < pHead2->val) {
+            pMergeHead = pHead1;
+            pMergeHead->next = Merge(pHead1->next, pHead2);
+        }
+        else {
+            pMergeHead = pHead2;
+            pMergeHead->next = Merge(pHead1, pHead2->next);
+        }
+        
+        return pMergeHead;
+    }
+};
