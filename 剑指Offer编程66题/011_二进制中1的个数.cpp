e070426364601823b3bd8045f6fed1ea637eac87
201419 <1275460343@qq.com>
2020-06-27 17:14:52 +0800

    20200627-01

--- 剑指Offer编程66题/011_二进制中1的个数.cpp
+++ 剑指Offer编程66题/011_二进制中1的个数.cpp
@@ -1,3 +1,8 @@
+// 题目描述
+
+// 输入一个整数，输出该数二进制表示中1的个数。其中负数用补码表示
+
+
 class Solution {
 public:
      int  NumberOf1(int n) {
--- 剑指Offer编程66题/020_包含min函数的栈.cpp
+++ 剑指Offer编程66题/020_包含min函数的栈.cpp
@@ -1,3 +1,9 @@
+/* 题目描述
+
+定义栈的数据结构，请在该类型中实现一个能够得到栈最小元素的min函数。
+*/
+
+
 class Solution {
 public:
     void push(int value) {
