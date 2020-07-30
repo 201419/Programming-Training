cde0dcb707ddf2883c2f6f84b11e331475fca619
YangShu <yangshu1109@foxmail.com>
2020-07-30 11:36:52 +0800

    Update 0021-0030.cpp

--- Leetcode/0021-0030.cpp
+++ Leetcode/0021-0030.cpp
@@ -27,6 +27,33 @@ public:
 
 // ---------------------------------------------------------------------------
 
+// https://leetcode-cn.com/problems/generate-parentheses/
+// 22. 括号生成
+
+class Solution {
+public:
+    vector<string> ans;
+    int N;
+    
+    void dfs(int l,int r,string has) {
+        if (r > l)  return;
+        if (l > N)  return;
+        if (l == r && r == N) {
+            ans.push_back(has);
+            return;
+        }
+        dfs(l + 1, r, has + "(");
+        dfs(l, r + 1, has + ")");
+    }
+    
+    vector<string> generateParenthesis(int n) {
+        N = n;
+        if (!N) return {};
+        dfs(0, 0, "");
+        return ans;
+    }
+};
+
 // ---------------------------------------------------------------------------
 
 // https://leetcode-cn.com/problems/merge-k-sorted-lists/
@@ -66,9 +93,150 @@ public:
     }
 };
 
-// ---------------------------------------------------------------------------
+// ===================
+
+class Solution {
+private:
+    ListNode* merge(ListNode* list1, ListNode* list2) {
+        if (list1 == NULL && list2 == NULL) return NULL;
+        else if (list1 != NULL && list2 == NULL) return list1;
+        else if (list1 == NULL && list2 != NULL) return list2;
+        
+        ListNode *head, *p;
+        if (list1->val <= list2->val) {
+            head = list1;
+            list1 = list1->next;
+        } else {
+            head = list2;
+            list2 = list2->next;
+        }
+        p = head;                     
+        while (list1 != NULL && list2 != NULL) {
+            if (list1->val <= list2->val) {
+                p->next = list1;
+                list1 = list1->next;
+                p = p->next;
+            } else {
+                p->next = list2;
+                list2 = list2->next;
+                p = p->next;
+            }
+        }
+        while (list1 != NULL) {
+            p->next = list1;      
+            list1 = list1->next;
+            p = p->next; 
+        }
+        while (list2 != NULL) {
+            p->next = list2;
+            list2 = list2->next;
+            p = p->next;  
+        }
+        p->next = NULL;
+        return head;
+    }
+    
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if (lists.size() == 0) return NULL;
+        
+        ListNode *result = lists[0];
+        for (int i = 1; i < lists.size(); i++) {
+            result = merge(result, lists[i]);
+        }
+        return result;
+    }
+};
 
 // ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/swap-nodes-in-pairs/
+// 24. 两两交换链表中的节点
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (head == NULL || head->next == NULL) return head;
+
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+
+        ListNode* prevNode = dummy;
+        ListNode* currentNode = head;
+        ListNode* nextNode = head->next;
+        while (currentNode != NULL && nextNode != NULL) {
+            prevNode->next = nextNode;
+            currentNode->next = nextNode->next;
+            nextNode->next = currentNode;
+
+            prevNode = currentNode;
+            currentNode = currentNode->next;
+            if (currentNode != NULL) nextNode = currentNode->next;
+        }
+
+        head = dummy->next;
+        delete dummy;
+        return head;
+    }
+};
+
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/reverse-nodes-in-k-group/
+// 25. K 个一组翻转链表
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    pair<ListNode*, ListNode*> myReverse(ListNode* head, ListNode* tail) {
+        ListNode* prev = tail->next;
+        ListNode* pNode = head;
+        while (prev != tail) {
+            ListNode* nextNode = pNode->next;
+            pNode->next = prev;
+            prev = pNode;
+            pNode = nextNode;
+        }
+        return {tail, head};
+    }
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* hair = new ListNode(0);
+        hair->next = head;
+        ListNode* prev = hair;
+
+        while (head) {
+            ListNode* tail = prev;
+            for (int i = 0; i < k; i++) {
+                tail = tail->next;
+                if (!tail)  return hair->next;
+            }
+            ListNode* nextNode = tail->next;
+            auto result = myReverse(head, tail);
+            head = result.first; tail = result.second;
+            prev->next = head;
+            tail->next = nextNode;
+            prev = tail;
+            head = nextNode;
+        }
+
+        return hair->next;
+    }
+};
 
 // ---------------------------------------------------------------------------
 
@@ -116,8 +284,15 @@ public:
 
 // ---------------------------------------------------------------------------
 
-// ---------------------------------------------------------------------------
+// https://leetcode-cn.com/problems/implement-strstr/
+// 28. 实现 strStr()
 
 // ---------------------------------------------------------------------------
 
+// https://leetcode-cn.com/problems/divide-two-integers/
+// 29. 两数相除
 
+// ---------------------------------------------------------------------------
+
+// https://leetcode-cn.com/problems/substring-with-concatenation-of-all-words/
+// 30. 串联所有单词的子串
