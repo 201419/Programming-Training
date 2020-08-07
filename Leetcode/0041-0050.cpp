683e1c5a820e508a4fcd5f8b8c63fb6209b5b04c
YangShu <yangshu1109@foxmail.com>
2020-08-07 09:55:11 +0800

    Update 0041-0050.cpp

--- Leetcode/0041-0050.cpp
+++ Leetcode/0041-0050.cpp
@@ -1,10 +1,47 @@
+// https://leetcode-cn.com/problems/first-missing-positive/
+// 41. 缺失的第一个正数
 
-// 41.
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                swap(nums[nums[i] - 1], nums[i]);
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] != i + 1) return i + 1;
+        }
+        return n + 1;
+    }
+};
 
 // ---------------------------------------------------------------------------
 
+// https://leetcode-cn.com/problems/trapping-rain-water/
+// 42. 接雨水
 
-// 42.
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        if (height.size() < 3) return 0;
+        vector<int> leftmax(height.size()), rightmax(height.size());
+        leftmax[0] = height[0];
+        for (int i = 1; i < height.size() - 1; i++) {
+            leftmax[i] = max(leftmax[i-1], height[i]);
+        }
+        rightmax[height.size()-1] = height[height.size()-1];
+        for (int i = height.size() - 2; i > 0; i--) {
+            rightmax[i] = max(rightmax[i+1], height[i]);
+        }
+        int ans = 0;
+        for (int i = 1; i < height.size() - 1; i++) {
+            ans += min(leftmax[i], rightmax[i]) - height[i];
+        }
+        return ans;
+    }
+};
 
 // ---------------------------------------------------------------------------
 
@@ -111,7 +148,25 @@ public:
 
 // ---------------------------------------------------------------------------
 
-// 45.
+// https://leetcode-cn.com/problems/jump-game-ii/
+// 45. 跳跃游戏 II
+
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int step = 0;
+        for (int end = 0, maxPos = 0, i = 0; i < nums.size() - 1; ++i) {
+            if (maxPos >= i) {
+                maxPos = max(maxPos, i + nums[i]);
+                if (i == end) {
+                    end = maxPos;
+                    ++step;
+                }
+            }
+        }
+        return step;
+    }
+};
 
 // ---------------------------------------------------------------------------
 
@@ -155,13 +210,83 @@ public:
 // https://leetcode-cn.com/problems/rotate-image/
 // 48. 旋转图像
 
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        for (int begin = 0, end = matrix.size() - 1; begin < end; begin++, end--) {
+            for (int i = 0; i < end - begin; i++) {
+                int tmp = matrix[begin][begin+i];
+                matrix[begin][begin+i] = matrix[end-i][begin];
+                matrix[end-i][begin] = matrix[end][end-i];
+                matrix[end][end-i] = matrix[begin+i][end];
+                matrix[begin+i][end] = tmp;
+            }
+        }
+    }
+};
+
 // ---------------------------------------------------------------------------
 
 // https://leetcode-cn.com/problems/group-anagrams/
 // 49. 字母异位词分组
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> res;  
+        int sub = 0;
+        string tmp;
+        unordered_map<string, int> work;
+        for (auto str:strs) {
+            tmp = str;
+            sort(tmp.begin(), tmp.end());
+            if (work.count(tmp)) {
+               res[work[tmp]].push_back(str);
+            } else {
+                vector<string> vec(1, str);
+                res.push_back(vec);
+                work[tmp] = sub++;
+            }
+        }
+        return res;
+    }
+};
 
 // ---------------------------------------------------------------------------
 
 // https://leetcode-cn.com/problems/powx-n/
 // 50. Pow(x, n)
 
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long N = n;
+        return N >= 0 ? quickMul(x, N) : 1.0 / quickMul(x, -N);
+    }
+
+    double quickMul(double x, long long N) {
+        double ans = 1.0;
+        double x_contribute = x;
+        while (N > 0) {
+            if (N & 1 == 1)  ans *= x_contribute;
+            x_contribute *= x_contribute;
+            N = N >> 1;
+        }
+        return ans;
+    }
+};
+
+// ------------------
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long N = n;
+        return N >= 0 ? quickMul(x, N) : 1.0 / quickMul(x, -N);
+    }
+    double quickMul(double x, long long N) {
+        if (N == 0)  return 1.0;
+        double y = quickMul(x, N>>1);
+        return (N&1) == 0 ? y*y : y*y*x;
+    }
+};
